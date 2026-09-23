dfaa67fe7c2a675b42d94d82bc6511fb7d1f36fc
Whytenko <whywhytenko@gmail.com>
2026-09-23 15:33:02 +0300

    Обновить версию кэша (v3) для значков

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "polevoy-konspekt-v2";
+var CACHE_NAME = "polevoy-konspekt-v3";
 var CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (event) {
