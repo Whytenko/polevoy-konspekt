16bc6d03dba4c2bc2cbe9635337ed7f6193746f3
Whytenko <whywhytenko@gmail.com>
2026-09-25 14:23:18 +0300

    Сделать экзамены многоразовыми: пересдача, история попыток (кэш v7)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "polevoy-konspekt-v6";
+var CACHE_NAME = "polevoy-konspekt-v7";
 var CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (event) {
